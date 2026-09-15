5a1f5ca1767842a943183a974453734305d60a8c
Dizz7 <162482518+Dizz7@users.noreply.github.com>
2026-09-15 09:56:13 -0300

    Update PacienteService.java
    Se actualiza PacienteService.java con la lógica del negocio.

--- micro_b/src/main/java/cl/duoc/micro_b/service/PacienteService.java
+++ micro_b/src/main/java/cl/duoc/micro_b/service/PacienteService.java
@@ -2,38 +2,25 @@ package cl.duoc.micro_b.service;
 
 import org.springframework.stereotype.Service;
 import cl.duoc.micro_b.model.Paciente;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-// Lógica del negocio relacionada con los pacientes
+
+// Lógica del negocio relacionada con lis pacientes, como obtener la lista de pacientes, buscar por ID, etc.
 
 @Service
-public class PacienteService {
-    
-    private List<Paciente> pacientes = new ArrayList<>();
-
-    public PacienteService() {
-        // Se agregan pacientes de ejemplo al servicio
-        pacientes.add(new Paciente(1, "Juan Pérez", "12.345.678-9", "Hipertensión crónica", "Control general de presión"));
-        pacientes.add(new Paciente(2, "María Gómez", "13.456.789-K", "Asma alérgica", "Crisis obstructiva leve"));
-        pacientes.add(new Paciente(3, "Carlos Soto", "14.567.890-1", "Sano / Sin antecedentes", "Certificado médico laboral"));
-        pacientes.add(new Paciente(4, "Ana Torres", "15.678.901-2", "Diabetes Tipo 2", "Ajuste de dosis de insulina"));
-        pacientes.add(new Paciente(5, "Pedro Rojas", "16.789.012-3", "Artritis reumatoide", "Revisión de exámenes de sangre"));
-        pacientes.add(new Paciente(6, "Sofía Valdés", "17.890.123-4", "Migraña crónica", "Receta retenida para tratamiento"));
-        pacientes.add(new Paciente(7, "Luis Morales", "18.901.234-5", "Gastritis aguda", "Endoscopia de control"));
-        pacientes.add(new Paciente(8, "Carmen Riquelme", "19.012.345-6", "Sana", "Examen preventivo de medicina (EMP)"));
-    }
-
+public interface PacienteService {
     // Métodos para acceder a los pacientes
-    public List<Paciente> getAllPacientes() {
-        return pacientes;
-    }
+        List<Paciente> getAllPacientes();
 
     // Método para obtener un paciente por su ID
-    public Paciente getPacienteById(int id) {
-        return pacientes.stream()
-                .filter(p -> p.getId() == id)
-                .findFirst()
-                .orElse(null);
-    }
+        Optional<Paciente> getPacienteById(Long id);
+
+    // Métodos para Crear, Actualizar y Eliminar pacientes
+        Paciente createPaciente(Paciente paciente);
+        Paciente updatePaciente(Long id, Paciente paciente);
+        void deletePaciente(Long id);
+
 }
+
+
